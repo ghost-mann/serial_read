33aee345c8004e9781e6808db086bde3c03c48a3
ghost-mann <iamaob8@protonmail.com>
2026-01-07 13:21:07 +0300

    feat: added setup function, loop function monitors if users sends data and reads whole numbers and saves it in brightness variable

--- src/analog_serial_real.cpp
+++ src/analog_serial_real.cpp
@@ -4,3 +4,13 @@
 int analog_ledPin = 9;
 int brightness = 0;
 
+void analog_serial_setup() {
+    pinMode(analog_ledPin,OUTPUT);
+    Serial.begin(9600);
+}
+
+void analog_serial_loop() {
+    if (Serial.available() > 0) {
+        brightness = Serial.parseInt();
+    }
+}
